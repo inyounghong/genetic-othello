8d6506a8be97b2f6260c80b8992feecdbcc73699
Daggtex <dmk254@cornell.edu>
2017-04-06 15:01:24 -0400

    initial file structure

--- src/Othello.java
+++ src/Othello.java
@@ -1,8 +1,29 @@
 
 public class Othello {
+	
+	private Piece[][] board;
 
 	public Othello() {
-		// TODO Auto-generated constructor stub
+		// TODO
+	}
+	
+	public Othello(Piece[][] b) {
+		//TODO
+	}
+	
+	public Othello makeMove(String m) {
+		//TODO
+		return null;
+	}
+	
+	public String[] getValidMoves() {
+		//TODO
+		return null;
+	}
+	
+	public State getGameState() {
+		//TODO
+		return null;
 	}
 
 }
---
+++ src/State.java
@@ -0,0 +1,4 @@
+
+public enum State {
+	B, W, DRAW, NONE
+}
